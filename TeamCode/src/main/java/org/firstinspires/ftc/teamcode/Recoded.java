b1b5b414ac819645009025dc4da81a9b27c8c73c
jfishCoder <james.salmon722@gmail.com>
2025-09-23 20:19:00 -0400

    9/23A/2025
    - Updated EPRALib to 1.3.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recoded.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recoded.java
@@ -2,9 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.epra.epralib.ftclib.control.Controller;
 import com.epra.epralib.ftclib.location.IMUExpanded;
+import com.epra.epralib.ftclib.location.Odometry;
+import com.epra.epralib.ftclib.location.Pose;
+import com.epra.epralib.ftclib.math.geometry.Angle;
+import com.epra.epralib.ftclib.math.geometry.Point;
 import com.epra.epralib.ftclib.movement.DcMotorExFrame;
 import com.epra.epralib.ftclib.movement.DriveTrain;
 import com.epra.epralib.ftclib.movement.MotorController;
+import com.epra.epralib.ftclib.movement.PIDController;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -17,6 +22,8 @@ import java.util.HashMap;
 @TeleOp
 public class Recoded extends LinearOpMode {
 
+    private final Pose START_POSE = new Pose(new Point(0, 0), new Angle(0.0));
+
     private MotorController frontLeft;
     private MotorController frontRight;
     private MotorController backLeft;
@@ -26,6 +33,7 @@ public class Recoded extends LinearOpMode {
     private HashMap<String, MotorController> nonDriveMotors;
 
     private IMUExpanded imu;
+    private Odometry odometry;
 
     private Controller controller1;
     private Controller controller2;
@@ -42,6 +50,8 @@ public class Recoded extends LinearOpMode {
             //Initializing the DriveTrain
             drive = new DriveTrain(new MotorController[] {frontLeft, frontRight, backLeft, backRight},
                     new DriveTrain.Orientation[] {DriveTrain.Orientation.LEFT_FRONT, DriveTrain.Orientation.RIGHT_FRONT, DriveTrain.Orientation.LEFT_BACK, DriveTrain.Orientation.RIGHT_BACK},
+                    odometry::getPose,
+                    odometry::getDeltaPose,
                     DriveTrain.DriveType.MECANUM);
 
             //Setting up the MotorControllers that are not part of the DriveTrain
@@ -59,6 +69,15 @@ public class Recoded extends LinearOpMode {
             IMU tempIMU = hardwareMap.get(IMU.class, "imu 1");
             tempIMU.initialize(new IMU.Parameters(orientationOnRobot));
             imu = new IMUExpanded(tempIMU);
+
+            //Setting up odometry
+            odometry = new Odometry(frontLeft, backLeft, frontRight,
+                    new Point(7.92784216, 3.75),
+                    new Point(-8, 3.75),
+                    new Point(0, 2.0),
+                    imu,
+                    START_POSE
+            );
 
             //Setting up the controller
             controller1 = new Controller(gamepad1, 0.05f, "1");
@@ -84,6 +103,7 @@ public class Recoded extends LinearOpMode {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            PIDController.update();
 
             //Uses the joysticks to drive the robot with fieldOrientedMecanumDrive
             drive.fieldOrientedMecanumDrive(controller1.analogDeadband(Controller.Key.RIGHT_STICK_X), controller1.analogDeadband(Controller.Stick.LEFT_STICK), imu.getYaw());
